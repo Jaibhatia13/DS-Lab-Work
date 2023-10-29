f2359258b10357b4a910f03bc4b818521469965a
Jaibhatia13 <148218744+Jaibhatia13@users.noreply.github.com>
2023-10-29 23:54:55 +0530

    Update Expt61_graphCreation.cpp

--- Expt61_graphCreation.cpp
+++ Expt61_graphCreation.cpp
@@ -7,8 +7,7 @@ void addEdge(vector<int> adj[], int u, int v)
 	adj[v].push_back(u);
 }
 
-void DFSUtil(int u, vector<int> adj[],
-					vector<bool> &visited)
+void DFSUtil(int u, vector<int> adj[],vector<bool> &visited)
 {
 	visited[u] = true;
 	cout << u << " ";
