1420ed5d533ed0e4e662703436b8a04dc23b5bc3
Jaibhatia13 <148218744+Jaibhatia13@users.noreply.github.com>
2023-10-30 06:42:19 +0530

    Update Expt62_BFS.cpp

--- Expt62_BFS.cpp
+++ Expt62_BFS.cpp
@@ -1,51 +1,58 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-vector<int> bfsOfGraph(int V, vector<int> adj[])
-{
+class Graph {
+    int V; // Number of vertices
+    vector<vector<int>> adj; // Adjacency list
 
-    queue<int> q;
-    q.push(0);
-    bool visited[V] = {0};
-    vector<int> ans;
-    visited[0] = 1;
-    while (!q.empty())
-    {
-        int front = q.front();
-        q.pop();
-        ans.push_back(front);
-        for (auto neighbour : adj[front])
-        {
-            if (!visited[neighbour])
-            {
-                visited[neighbour] = 1;
-                q.push(neighbour);
+public:
+    Graph(int v) : V(v) {
+        adj.resize(V);
+    }
+
+    // Function to add an edge to the graph
+    void addEdge(int u, int v) {
+        adj[u].push_back(v);
+    }
+
+    // Function to perform BFS traversal
+    void BFS(int start) {
+        vector<bool> visited(V, false); // To keep track of visited vertices
+        queue<int> q;
+
+        visited[start] = true;
+        q.push(start);
+
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+            cout << current << " ";
+
+            for (int neighbor : adj[current]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
             }
         }
     }
+};
 
-    return ans;
-}
+int main() {
+    Graph g(6); // Create a graph with 6 vertices
 
-int main()
-{
+    // Add edges to the graph
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(2, 4);
+    g.addEdge(3, 5);
 
-    int V, E;
-    cin >> V >> E;
-    vector<int> adj[V];
-    for (int i = 0; i < E; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
+    cout << "BFS Traversal starting from vertex 0: ";
+    g.BFS(0);
 
-    vector<int> ans = bfsOfGraph(V, adj);
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i] << " ";
-    }
-    cout << endl;
     return 0;
 }
